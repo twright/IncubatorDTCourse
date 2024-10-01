8ca3c9138cf7d4868d853b47240eb7e8759dbd56
Claudio Gomes <cla.gms@gmail.com>
2024-10-01 15:04:28 +0200

    Run the tests

--- run_tests.ps1
+++ run_tests.ps1
@@ -3,4 +3,5 @@ $Env:PYDEVD_DISABLE_FILE_VALIDATION=1
 
 Push-Location .\0-Pre-requisites
 jupyter nbconvert --to notebook --execute --inplace 1-Git.ipynb;
+jupyter nbconvert --to notebook --execute --inplace 2-Docker.ipynb;
 Pop-Location
