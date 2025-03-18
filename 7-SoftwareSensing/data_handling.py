3bb43bdae3456b8dba161db13dec85c437048a3c
Claudio Gomes <cla.gms@gmail.com>
2025-03-18 14:58:31 +0100

    Particle filter example done

--- 7-SoftwareSensing/data_handling.py
+++ 7-SoftwareSensing/data_handling.py
@@ -3,6 +3,8 @@
 import math
 import pandas
 import numpy as np
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 def from_ns_to_s(time_ns):
     return time_ns / 1e9
@@ -93,4 +95,91 @@ def load_data(filepath, heater_voltage, heater_current, events=None, desired_tim
         event_data = load_timestamped_data(events, desired_timeframe, time_unit, normalize_time, convert_to_seconds)
 
     return derive_data(data, heater_voltage, heater_current, events=event_data), event_data
+
+
+def plotly_incubator_data(data, compare_to=None, heater_T_data=None, events=None,
+                          overlay_heater=True, show_actuators=False, show_sensor_temperatures=False,
+                          show_hr_time=False):
+    nRows = 2
+    titles = ["Incubator Temperature (°C)", "Room Temperature (°C)"]
+    if show_actuators:
+        nRows += 1
+        titles.append("Actuators")
+    if heater_T_data is not None:
+        nRows += 1
+        titles.append("Heatbed Temperature (°C)")
+
+    x_title = "Timestamp" if show_hr_time else "Time (s)"
+
+    time_field = "timestamp_ns" if show_hr_time else "time"
+
+    fig = make_subplots(rows=nRows, cols=1, shared_xaxes=True,
+                        x_title=x_title,
+                        subplot_titles=titles)
+
+    if show_sensor_temperatures:
+        fig.add_trace(go.Scatter(x=data[time_field], y=data["t2"], name="t2 (right)"), row=1, col=1)
+        fig.add_trace(go.Scatter(x=data[time_field], y=data["t3"], name="t3 (top)"), row=1, col=1)
+
+    fig.add_trace(go.Scatter(x=data[time_field], y=data["average_temperature"], name="avg_T"), 
+                  row=1, col=1)
+    if overlay_heater:
+        fig.add_trace(go.Scatter(x=data[time_field], y=[40 if b else 30 for b in data["heater_on"]], name="heater_on"), row=1, col=1)
+
+    if events is not None:
+        for i, r in events.iterrows():
+            # Get the closest timestamp_ns to the event time
+            closest_ts = min(data[time_field], key=lambda x:abs(x-r[time_field]))
+            # Get the average temperature for that timestamp_ns
+            avg_temp = data.iloc[data.index[data[time_field] == closest_ts]]["average_temperature"].iloc[0]
+
+            fig.add_annotation(x=r[time_field], y=avg_temp,
+                               text=r["event"],
+                               showarrow=True,
+                               arrowhead=1)
+
+    if compare_to is not None:
+        for res in compare_to:
+            if "T" in compare_to[res]:
+                fig.add_trace(go.Scatter(x=compare_to[res][time_field], 
+                                         y=compare_to[res]["T"],
+                                         error_y={
+                                            "type" : 'data',
+                                            "array" : compare_to[res]["T_std_dev"] if "T_std_dev" in compare_to[res] else None,
+                                         },
+                                         name=f"avg_temp({res})"), row=1, col=1)
+            if "T_object" in compare_to[res]:
+                fig.add_trace(go.Scatter(x=compare_to[res][time_field], y=compare_to[res]["T_object"], name=f"T_object({res})"), row=1, col=1)
+
+    fig.add_trace(go.Scatter(x=data[time_field], y=data["t1"], name="room"), 
+                  row=2, col=1)
+
+    next_row = 3
+
+    if show_actuators:
+        fig.add_trace(go.Scatter(x=data[time_field], y=data["heater_on"], name="heater_on"), row=next_row, col=1)
+        fig.add_trace(go.Scatter(x=data[time_field], y=data["fan_on"], name="fan_on"), row=next_row, col=1)
+
+        if compare_to is not None:
+            for res in compare_to:
+                if "in_lid_open" in compare_to[res]:
+                    fig.add_trace(go.Scatter(x=compare_to[res][time_field], y=compare_to[res]["in_lid_open"], name=f"in_lid_open({res})"), row=next_row, col=1)
+
+        next_row += 1
+
+    if heater_T_data is not None:
+        for trace in heater_T_data:
+            fig.add_trace(go.Scatter(x=heater_T_data[trace][time_field], y=heater_T_data[trace]["T_heater"],
+                                     name=f"T_heater({trace})", 
+                                     error_y={
+                                        "type" : 'data',
+                                        "array" : heater_T_data[trace]["T_heater_std_dev"] if "T_heater_std_dev" in heater_T_data[trace] else None,
+                                    }
+                                    ), 
+                                    row=next_row, col=1)
+        next_row += 1
+
+    fig.update_layout()
+
+    return fig
 
